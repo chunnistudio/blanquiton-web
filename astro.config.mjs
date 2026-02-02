74ee29b727ae323c6d5ad24fc7776154a8afb293
Harpuia <zetsubou.harpuia@gmail.com>
2026-02-01 21:21:33 -0300

    feat: workflow despliegue web

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://blanquiton-web.github.io',
+    base: 'blanquiton-web',
     integrations: [vue()]
 });
